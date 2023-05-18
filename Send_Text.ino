51098e09a0631f4f7b4bfe855181ea43bb193867
DangKhoa-VT2-PTIT <125228780+DangKhoa-VT2-PTIT@users.noreply.github.com>
2023-05-18 15:31:55 +0700

    Update Send_Text.ino
    Final

--- Send_Text.ino
+++ Send_Text.ino
@@ -89,7 +89,7 @@ void smtpCallback(SMTP_Status status);
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
 WiFiMulti multi;
 #endif
-
+String teststr;
 void setup()
 {
 
@@ -209,7 +209,7 @@ void setup()
   message.subject = F("Test sending plain text Email");
   message.addRecipient(F("Someone"), RECIPIENT_EMAIL);
 
-  String textMsg = "This is simple plain text message";
+  String textMsg = teststr;
   message.text.content = textMsg;
 
   /** If the message to send is a large string, to reduce the memory used from internal copying  while sending,
@@ -326,6 +326,154 @@ void setup()
 
 void loop()
 {
+  while (Serial.available() == 0) {}     //wait for data available
+  teststr = Serial.readString();  //read until timeout
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Declare the Session_Config for user defined session credentials */
+  Session_Config config;
+
+  /* Set the session config */
+  config.server.host_name = SMTP_HOST;
+  config.server.port = SMTP_PORT;
+  config.login.email = AUTHOR_EMAIL;
+  config.login.password = AUTHOR_PASSWORD;
+
+  /** Assign your host name or you public IPv4 or IPv6 only
+   * as this is the part of EHLO/HELO command to identify the client system
+   * to prevent connection rejection.
+   * If host name or public IP is not available, ignore this or
+   * use generic host "mydomain.net".
+   *
+   * Assign any text to this option may cause the connection rejection.
+   */
+  config.login.user_domain = F("");
+
+  /* Set the NTP config time */
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = 3;
+  config.time.day_light_offset = 0;
+
+  /* Set the message headers */
+  message.sender.name = F("ESP Mail");
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = F("Test sending plain text Email");
+  message.addRecipient(F("Someone"), RECIPIENT_EMAIL);
+
+  String textMsg = teststr;
+  message.text.content = textMsg;
+
+  /** If the message to send is a large string, to reduce the memory used from internal copying  while sending,
+   * you can assign string to message.text.blob by cast your string to uint8_t array like this
+   *
+   * String myBigString = "..... ......";
+   * message.text.blob.data = (uint8_t *)myBigString.c_str();
+   * message.text.blob.size = myBigString.length();
+   *
+   * or assign string to message.text.nonCopyContent, like this
+   *
+   * message.text.nonCopyContent = myBigString.c_str();
+   *
+   * Only base64 encoding is supported for content transfer encoding in this case.
+   */
+
+  /** The Plain text message character set e.g.
+   * us-ascii
+   * utf-8
+   * utf-7
+   * The default value is utf-8
+   */
+  message.text.charSet = F("us-ascii");
+
+  /** The content transfer encoding e.g.
+   * enc_7bit or "7bit" (not encoded)
+   * enc_qp or "quoted-printable" (encoded)
+   * enc_base64 or "base64" (encoded)
+   * enc_binary or "binary" (not encoded)
+   * enc_8bit or "8bit" (not encoded)
+   * The default value is "7bit"
+   */
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  // If this is a reply message
+  // message.in_reply_to = "<parent message id>";
+  // message.references = "<parent references> <parent message id>";
+
+  /** The message priority
+   * esp_mail_smtp_priority_high or 1
+   * esp_mail_smtp_priority_normal or 3
+   * esp_mail_smtp_priority_low or 5
+   * The default value is esp_mail_smtp_priority_low
+   */
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+
+  // message.response.reply_to = "someone@somemail.com";
+  // message.response.return_path = "someone@somemail.com";
+
+  /** The Delivery Status Notifications e.g.
+   * esp_mail_smtp_notify_never
+   * esp_mail_smtp_notify_success
+   * esp_mail_smtp_notify_failure
+   * esp_mail_smtp_notify_delay
+   * The default value is esp_mail_smtp_notify_never
+   */
+  // message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* Set the custom message header */
+  message.addHeader(F("Message-ID: <abcde.fghij@gmail.com>"));
+
+  // For Root CA certificate verification (ESP8266 and ESP32 only)
+  // config.certificate.cert_data = rootCACert;
+  // or
+  // config.certificate.cert_file = "/path/to/der/file";
+  // config.certificate.cert_file_storage_type = esp_mail_file_storage_type_flash; // esp_mail_file_storage_type_sd
+  // config.certificate.verify = true;
+
+  // The WiFiNINA firmware the Root CA certification can be added via the option in Firmware update tool in Arduino IDE
+
+  /* Connect to server with the session config */
+
+  // Library will be trying to sync the time with NTP server if time is never sync or set.
+  // This is 10 seconds blocking process.
+  // If time synching was timed out, the error "NTP server time synching timed out" will show via debug and callback function.
+  // You can manually sync time by yourself with NTP library or calling configTime in ESP32 and ESP8266.
+  // Time can be set manually with provided timestamp to function smtp.setSystemTime.
+
+  /* Connect to the server */
+  if (!smtp.connect(&config))
+  {
+    ESP_MAIL_PRINTF("Connection error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
+    return;
+  }
+
+  /** Or connect without log in and log in later
+
+     if (!smtp.connect(&config, false))
+       return;
+
+     if (!smtp.loginWithPassword(AUTHOR_EMAIL, AUTHOR_PASSWORD))
+       return;
+  */
+
+  if (!smtp.isLoggedIn())
+  {
+    Serial.println("\nNot yet logged in.");
+  }
+  else
+  {
+    if (smtp.isAuthenticated())
+      Serial.println("\nSuccessfully logged in.");
+    else
+      Serial.println("\nConnected with no Auth.");
+  }
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    ESP_MAIL_PRINTF("Error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
+
+  // to clear sending result log
+  // smtp.sendingResult.clear();
 }
 
 /* Callback function to get the Email sending status */
